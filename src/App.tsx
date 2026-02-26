aa1e21c9a07ecffce94367dd230bdc55bc1d3704
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-26 12:24:06 +0300

    add controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,11 @@
+import ReactSimpleLogic from "./components/ReactSimple.logic"
 
-import './App.css';
-import Card from "./components/Card";
-import img from "./assets/sur.jpg"
-function App() {
-
-    
+const App = () => {
   return (
-    <>
-      <h1 className="flex items-center justify-between gap-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
-      {/* call  */}
-      <Card imgSrc={img} 
-       title="Surafel Mengist"
-        description="Software Engineer and Developer."
-        contact="Contact Me"
-      />
-    </>
-  );
+    <div>
+      <ReactSimpleLogic />
+    </div>
+  )
 }
 
 export default App
