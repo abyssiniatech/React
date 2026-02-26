afa3aa0f97a76bea8ef6e088bcc6651f48b0e089
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-26 11:47:04 +0300

    add card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
 
 import './App.css';
-import Counterprops from './components/Counterprops';
+import Card from "./components/Card";
+import img from "./assets/sur.jpg"
 function App() {
 
     
   return (
     <>
-      <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      <h1 className="flex items-center justify-between gap-4">Props Counter App</h1>
       {/* call  */}
-      <Counterprops count={0} />
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
+        <h1 className="text-2xl font-bold text-center p-4">Props Counter App</h1>
+      {/* call  */}
+      <Card imgSrc={img} 
+       title="Surafel Mengist"
+        description="Software Engineer and Developer."
+        contact="Contact Me"
+      />
     </>
   );
 }
